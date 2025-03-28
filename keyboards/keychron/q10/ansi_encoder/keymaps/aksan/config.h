dda34a50a68328cda6188e4bc2af1ad0dd3afab2
John Lee <shkblue@hotmail.com>
2025-03-27 23:46:59 -0700

    Disable permissive hold and adjust default tapping term and tapping term for HRM keys. Disallow auto shift of symbols.

--- keyboards/keychron/q10/ansi_encoder/keymaps/aksan/config.h
+++ keyboards/keychron/q10/ansi_encoder/keymaps/aksan/config.h
@@ -1,11 +1,13 @@
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
-#define PERMISSIVE_HOLD
+// Disabling PERMISSIVE_HOLD due to too many misfires due to timing of finger release
+// #define PERMISSIVE_HOLD
+
 #define CHORDAL_HOLD
 
 // Timeout for caps word to turn off when no keys are pressed
@@ -19,3 +21,6 @@
 #define AUTO_SHIFT_REPEAT
 // Do not auto shift alpha characters due to HRM
 #define NO_AUTO_SHIFT_ALPHA
+// Do not auto shift symbols because :; is used for HRM and cannot be used for auto shift,
+// so don't want to auto shift other symbols for consistency
+#define NO_AUTO_SHIFT_SYMBOLS 
--- keyboards/keychron/q10/ansi_encoder/keymaps/aksan/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/aksan/keymap.c
@@ -33,12 +33,14 @@ enum layers{
 #define ALT_S LALT_T(KC_S)
 #define CTL_D LCTL_T(KC_D)
 #define SFT_F LSFT_T(KC_F)
+#define HYP_G HYPR_T(KC_G)
 
 // Right-hand home row mods
 #define SFT_J RSFT_T(KC_J)
 #define CTL_K RCTL_T(KC_K)
 #define ALT_L RALT_T(KC_L)
 #define GUI_SCLN RGUI_T(KC_SCLN)
+#define HYP_H HYPR_T(KC_H)
 
 // Tap dance declarations
 enum {
@@ -74,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,   KC_INS,             KC_DEL,
         _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,       KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,  KC_EQL,   TD_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,       KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  CW_TOGG,  GUI_A,    ALT_S,    CTL_D,    SFT_F,    KC_G,       KC_H,     SFT_J,    CTL_K,    ALT_L,    GUI_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+        _______,  CW_TOGG,  GUI_A,    ALT_S,    CTL_D,    SFT_F,    HYP_G,      HYP_H,    SFT_J,    CTL_K,    ALT_L,    GUI_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   MO(WIN_FN),                     KC_SPC,             KC_RALT,                       KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -124,7 +126,21 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TD_BSPC:
             // Shorten tapping term for backspace tap dance, to make it easier to backspace single character
-            return 160;
+            return 180;
+        case GUI_A:
+        case GUI_SCLN:
+            // Windows key is not used frequently, so increasing tapping term to avoid hold misfires
+            return 250;
+        case CTL_D:
+        case SFT_F:
+        case SFT_J:
+        case CTL_K:
+            // Decreasing tapping term for these HRM keys because they're frequently used and permissive hold disabled
+            return 150;
+        case HYP_G:
+        case HYP_H:
+            // Hyper key is not used frequently, so increasing tapping term to avoid hold misfires
+            return 250;
         default:
             return TAPPING_TERM;
     }
